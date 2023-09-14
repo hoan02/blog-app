fcfdf169f575f76b9ee82b66a2971a7be5dd481e
hoanit02 <hoanvipboi1@gmail.com>
2023-09-14 17:26:20 +0700

    config 'globals.css'

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,38 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href="/">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          suscipit dignissimos in aspernatur corrupti vel magnam adipisci alias
+          nam? Laudantium dolorum ea aspernatur numquam reprehenderit voluptates
+        </h1>
+        est eaque voluptatibus vitae? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Doloribus suscipit dignissimos in aspernatur corrupti
+        vel magnam adipisci alias nam? Laudantium dolorum ea aspernatur numquam
+        reprehenderit voluptates est eaque voluptatibus vitae? Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Doloribus suscipit dignissimos in
+        aspernatur corrupti vel magnam adipisci alias nam? Laudantium dolorum ea
+        aspernatur numquam reprehenderit voluptates est eaque voluptatibus
+        vitae? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Doloribus suscipit dignissimos in aspernatur corrupti vel magnam
+        adipisci alias nam? Laudantium dolorum ea aspernatur numquam
+        reprehenderit voluptates est eaque voluptatibus vitae? Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Doloribus suscipit dignissimos in
+        aspernatur corrupti vel magnam adipisci alias nam? Laudantium dolorum ea
+        aspernatur numquam reprehenderit voluptates est eaque voluptatibus
+        vitae? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Doloribus suscipit dignissimos in aspernatur corrupti vel magnam
+        adipisci alias nam? Laudantium dolorum ea aspernatur numquam
+        reprehenderit voluptates est eaque voluptatibus vitae? Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Doloribus suscipit dignissimos in
+        aspernatur corrupti vel magnam adipisci alias nam? Laudantium dolorum ea
+        aspernatur numquam reprehenderit voluptates est eaque voluptatibus
+        vitae?
+      </Link>
+    </div>
+  );
 }
