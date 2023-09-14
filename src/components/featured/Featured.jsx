7855904dfe3087d92d56b7ab9380aef3a0ab4ae2
hoanit02 <hoanvipboi1@gmail.com>
2023-09-14 22:15:21 +0700

    featured

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, HoanCuTe here!</b> Discover my stories and create ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image className={styles.image} src="/p1.jpeg" alt="" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero quia
+            sapiente eum corporis, dolore officia odit ex explicabo aut modi
+            vitae aperiam non adipisci doloremque perferendis similique ipsum
+            laboriosam maxime!
+          </p>
+          <button className={styles.button}>Read more</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
